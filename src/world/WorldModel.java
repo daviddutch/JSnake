9355aafc3ef0da143444c3e5bfe85e720a42aac3
david.dutch <david.dutch@gmail.com>
2010-11-12 11:48:58 +0100

    Base of model

--- src/world/WorldModel.java
+++ src/world/WorldModel.java
@@ -1,5 +1,98 @@
 package world;
 
-public class WorldModel {
+import java.util.Locale;
+import java.util.Observable;
 
+
+
+public class WorldModel extends Observable {
+  public enum WorldEvents {
+    CONFIG_CHANGED, STEP_FORWARD;
+  }
+  public enum GameState {
+    STOP, PLAY, PAUSE, GAME_OVER;
+  }
+  private int    score=0;
+  private int    speed=4;
+  private int    [][] gameGrid;
+  private GameState   state;
+  private Locale locale;
+  
+  
+  public WorldModel(){
+    setLocale(Locale.getDefault());
+    state = GameState.STOP;
+  }
+  /**
+   * @param width grid width
+   * @param height grid height
+   */
+  public void setGridDimensions(int width, int height) {
+    
+    
+  }
+  /**
+   * @param speed the speed to set
+   */
+  public void setSpeed(int speed) {
+    this.speed = speed;
+    setChanged();
+    notifyObservers(WorldEvents.CONFIG_CHANGED);
+  }
+  /**
+   * @return the speed
+   */
+  public int getSpeed() {
+    return speed;
+  }
+  /**
+   * @param score the score to set
+   */
+  public void setScore(int score) {
+    this.score = score;
+    setChanged();
+    notifyObservers(WorldEvents.CONFIG_CHANGED);
+  }
+  /**
+   * @return the score
+   */
+  public int getScore() {
+    return score;
+  }
+  /**
+   * @param locale the locale to set
+   */
+  public void setLocale(Locale locale) {
+    setChanged();
+    notifyObservers(WorldEvents.CONFIG_CHANGED);
+    this.locale = locale;
+  }
+  /**
+   * @return the locale
+   */
+  public Locale getLocale() {
+    return locale;
+  }
+  /**
+   * @param state the state to set
+   */
+  public void setState(GameState state) {
+    setChanged();
+    notifyObservers(WorldEvents.CONFIG_CHANGED);
+    this.state = state;
+  }
+  /**
+   * @return the state
+   */
+  public GameState getState() {
+    return state;
+  }
+  /**
+   * @return the gameGrid
+   */
+  public int    [][] getGameGrid() {
+    return gameGrid.clone(); //TODO : Deep clone
+  }
+  
+  
 }
