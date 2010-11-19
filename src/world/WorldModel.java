cc4bbae87e01491d9730dc17bbc715648aaae8eb
kean <kean@mariok08>
2010-11-19 12:53:07 +0100

    stepForward first implementation, still have to finish.

--- src/world/WorldModel.java
+++ src/world/WorldModel.java
@@ -1,7 +1,9 @@
 package world;
 
+import java.util.LinkedList;
 import java.util.Locale;
 import java.util.Observable;
+import java.util.Queue;
 
 
 
@@ -12,25 +14,36 @@ public class WorldModel extends Observable {
   public enum GameState {
     STOP, PLAY, PAUSE, GAME_OVER;
   }
+  public enum Direction {
+	UP, DOWN, LEFT, RIGHT;
+  }
   private int    score=0;
   private int	 cntEaten=0;
   private int    speed=4;
+  private int gridWidth=100;
+  private int gridHeight=100;
   private int    [][] gameGrid;
   private GameState   state;
   private Locale locale;
-  
+  private LinkedList<GridPoint> snake=new LinkedList<GridPoint>();
+  private Direction nextDirection = Direction.LEFT;
+  private GridPoint insect = new GridPoint(0, 0);
   
   public WorldModel(){
     setLocale(Locale.getDefault());
     state = GameState.STOP;
+    snake.addFirst(new GridPoint(gridWidth/2, gridHeight/2));
+    snake.addFirst(new GridPoint(gridWidth/2-1, gridHeight/2));
+    snake.addFirst(new GridPoint(gridWidth/2-2, gridHeight/2));
+    snake.addFirst(new GridPoint(gridWidth/2-2, gridHeight/2-1));
+    snake.addFirst(new GridPoint(gridWidth/2-2, gridHeight/2-2));
   }
   /**
    * @param width grid width
    * @param height grid height
    */
   public void setGridDimensions(int width, int height) {
-    
-    
+    gridWidth=width; gridHeight=height;
   }
   /**
    * @param speed the speed to set
@@ -104,12 +117,63 @@ public class WorldModel extends Observable {
     return state;
   }
   /**
-   * @return the gameGrid
+   * @return the insect
    */
-  public int    [][] getGameGrid() {
-    return gameGrid.clone(); //TODO : Deep clone
+  public GridPoint getInsect(){
+	  return insect;
   }
+  /**
+   * @return the Queue representing the snake
+   */
+  public Queue<GridPoint> getSnake(){
+	  return snake;
+  }
+  
+  /**
+   * Move snake to its next position.
+   */
   public void stepForward() {
+	GridPoint crt = snake.getFirst();
+	GridPoint next;
+	
+	switch(nextDirection) {
+		case UP:
+			next = new GridPoint(crt.getX(), crt.getY()-1);
+			break;
+		case DOWN:
+			next = new GridPoint(crt.getX(), crt.getY()+1);
+			break;
+		case LEFT:
+			next = new GridPoint(crt.getX()-1, crt.getY());
+			break;
+		case RIGHT:
+			next = new GridPoint(crt.getX()+1, crt.getY());
+			break;
+		default:
+			next = new GridPoint(crt.getX(), crt.getY());
+	}
+	
+	//TODO: if next position is not valid return error value
+	if(		next.getX()<0 || next.getX()>=gridWidth ||	//next point out of bounds
+			next.getY()<0 || next.getX()>=gridHeight) {
+		return; //TODO: return error
+	}
+	
+	for(GridPoint p : snake) {	// snake eat its own tail
+		if(next.getX()==p.getX() && next.getY()==p.getY()) {
+			return; //TODO: return error
+		}
+	}
+	
+	snake.addFirst(next);
+	
+	if(next.getX()==insect.getX() && next.getY()==insect.getY()) {
+		//TODO: replace insect somewhere else
+	}
+	else {
+		snake.removeLast();
+	}
+	
     setChanged();
     notifyObservers(WorldEvents.STEP_FORWARD);
   }
