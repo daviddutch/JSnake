9f54ac0da5d62b5eb6c505d574c1fef19a5a1494
Vincent <Vincent@160.98.61.190>
2010-11-26 10:51:02 +0100

    Observer add

--- src/ihm/OptionDialog.java
+++ src/ihm/OptionDialog.java
@@ -1,21 +1,102 @@
 package ihm;
 
-import java.awt.Dimension;
-import java.awt.Toolkit;
+import java.awt.*;
+import java.util.Locale;
+import java.util.Observer;
+import java.util.ResourceBundle;
 
-import javax.swing.JDialog;
+import javax.swing.*;
 
 import world.WorldModel;
 
 public class OptionDialog extends JDialog {
   
+  WorldModel wm;
+  ResourceBundle bIdat;
+
+  Box panel = Box.createVerticalBox();
+  
+  JLabel lblTitle     = new JLabel("Snake 1.0");
+  JLabel lblLang      = new JLabel();
+  JLabel lblSpeed     = new JLabel();
+  JLabel lblColor     = new JLabel();
+  
+  ImageIcon imgSnake;
+  
+  JButton btGame      = new JButton("Start game");
+  
+  JComboBox boxLang = new JComboBox();
+  JComboBox boxColor = new JComboBox();
+  
+  
   public OptionDialog(WorldModel wm){
-    setTitle("Snake options !!");
-    setSize(400, 200);
+    this.wm = wm;
+    this.wm.addObserver((Observer) new OptionDialogObserver(this));
+       
+    panel.add(Box.createVerticalGlue());
+    panel.setMaximumSize(panel.getPreferredSize());
+    
+    imgSnake = new ImageIcon("images/logoSnake.png");
+        
+    boxLang.setPreferredSize(new Dimension(100,20));
+    boxLang.setMaximumSize(boxLang.getPreferredSize());
+    boxLang.addActionListener(new changeLanguage(this));
+    
+    boxColor.setPreferredSize(new Dimension(100,20));
+    boxColor.setMaximumSize(boxColor.getPreferredSize());
+    //boxColor.addActionListener(new changeColor(this));
+
+    setText();
+    
+    setTitle("Snake Game - Settings");
+    setSize(800, 600);
     Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
     Dimension frameSize = this.getSize();
     //--- Place la fenêtre de l'application
     setLocation((screenSize.width - frameSize.width) / 2,
     (screenSize.height - frameSize.height) / 5);
+    
+    panel.add(lblTitle);
+    panel.add(lblLang);
+    panel.add(boxLang);
+    panel.add(lblSpeed);
+    panel.add(lblColor);
+    panel.add(btGame);
+
+    getContentPane().add(panel);
+    
+  }
+  
+  public void setText(){
+    bIdat = ResourceBundle.getBundle("lang.Idat", wm.getLocale());
+    
+    lblLang.setText(bIdat.getString("language")+" : ");
+    lblSpeed.setText(bIdat.getString("speed")+" : ");
+    lblColor.setText(bIdat.getString("color")+" : ");
+    
+    boxLang.removeAllItems();
+    boxLang.removeAll();
+    boxLang.addItem(bIdat.getString("french"));
+    boxLang.addItem(bIdat.getString("english"));
+    boxLang.addItem(bIdat.getString("italian"));
+    
+  }
+
+  public void changeLang(int i) {
+    System.out.println("idLang:"+i);
+    switch(i){
+      case 0 :
+        wm.setLocale(new Locale((Locale.FRENCH.toString())));
+        break;
+      case 1 :
+        wm.setLocale(new Locale((Locale.ENGLISH.toString())));
+        break;
+      case 2 :
+        wm.setLocale(new Locale((Locale.ITALIAN.toString())));
+        break;
+      default:
+        break;
+    }
+    //setText();
   }
 }
