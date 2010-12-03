0c6cf7c797139d44d7638c189451b3fbf02858d7
kean <kean@mariok08>
2010-12-03 11:07:05 +0100

    nothing

--- src/ihm/StatsPanel.java
+++ src/ihm/StatsPanel.java
@@ -1,6 +1,8 @@
 package ihm;
 
 import java.awt.Color;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -11,32 +13,54 @@ import com.atticlabs.zonelayout.swing.ZoneLayout;
 import com.atticlabs.zonelayout.swing.ZoneLayoutFactory;
 
 public class StatsPanel extends JPanel {
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
+	
+	ResourceBundle bIdat;
 	JLabel lbScore = new JLabel();
 	JLabel lbCntEaten = new JLabel();
 	JLabel lbSpeed = new JLabel();
 	
-  public StatsPanel(WorldModel wm){
-    ZoneLayout layout = ZoneLayoutFactory.newZoneLayout();
-    setLayout(layout);
-    layout.addRow("a.......ab.b");
-    layout.addRow("c.......cd.d");
-    layout.addRow("e.......ef.f");
-    add(new JLabel("Score :"), "a");
-    add(lbScore, "b");
-    add(new JLabel("Nb manger :"), "c");
-    add(lbCntEaten, "d");
-    add(new JLabel("Vitesse :"), "e");
-    add(lbSpeed, "f");
-    
-    StatsWorldObserver wo = new StatsWorldObserver(this, wm);
-    wm.addObserver(wo);
-    
-    setSize(230, 200);
-    setPreferredSize(getSize());
-    setBackground(Color.GRAY);
-  }
+	public StatsPanel(WorldModel wm){
+		ZoneLayout layout = ZoneLayoutFactory.newZoneLayout();
+	    setLayout(layout);
+	    layout.addRow("a.......ab.b");
+		layout.addRow("c.......cd.d");
+		layout.addRow("e.......ef.f");
+	
+		JLabel lblScore = new JLabel();
+		JLabel lblCntEaten = new JLabel();
+		JLabel lblSpeed = new JLabel();
+		
+		add(lblScore, "a");
+		add(lbScore, "b");
+		add(lblCntEaten, "c");
+		add(lbCntEaten, "d");
+		add(new JLabel("Vitesse :"), "e");
+		add(lbSpeed, "f");
+		    
+	    StatsWorldObserver wo = new StatsWorldObserver(this, wm);
+	    wm.addObserver(wo);
+	    
+	    setSize(230, 200);
+	    setPreferredSize(getSize());
+	    setBackground(Color.GRAY);
+	}
+  /*
+	public void setText(){
+		bIdat = ResourceBundle.getBundle("lang.Idat", wm.getLocale());
+		
+		lblLang.setText(bIdat.getString("language")+" : ");
+		lblSpeed.setText(bIdat.getString("speed")+" : ");
+		lblColor.setText(bIdat.getString("color")+" : ");
+		
+		comboModel.update();
+		boxLang.revalidate();
+		
+		boxColor.removeAllItems();
+		boxColor.addItem(bIdat.getString("blue"));
+		boxColor.addItem(bIdat.getString("red"));
+		boxColor.addItem(bIdat.getString("pink"));    
+		boxColor.revalidate();
+	}
+	*/
 }
